c8329baff079cbd583db8fae09042a7a1db31d81
animesh goyal <animesh9@cs.utexas.edu>
2019-10-21 01:05:07 -0500

    first

---
+++ rcrs-adf-sample-master/src/adf/sample/module/algorithm/SamplePathPlanning.java
@@ -0,0 +1,246 @@
+package adf.sample.module.algorithm;
+
+import adf.agent.communication.MessageManager;
+import adf.agent.develop.DevelopData;
+import adf.agent.info.AgentInfo;
+import adf.agent.info.ScenarioInfo;
+import adf.agent.info.WorldInfo;
+import adf.agent.module.ModuleManager;
+import adf.agent.precompute.PrecomputeData;
+import adf.component.module.algorithm.PathPlanning;
+import rescuecore2.misc.collections.LazyMap;
+import rescuecore2.standard.entities.Area;
+import rescuecore2.worldmodel.Entity;
+import rescuecore2.worldmodel.EntityID;
+
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.io.*;
+import java.net.*;
+
+public class SamplePathPlanning extends PathPlanning {
+
+    private Map<EntityID, Set<EntityID>> graph;
+
+    private EntityID from;
+    private Collection<EntityID> targets;
+    private List<EntityID> result;
+
+    public SamplePathPlanning(AgentInfo ai, WorldInfo wi, ScenarioInfo si, ModuleManager moduleManager, DevelopData developData) {
+        super(ai, wi, si, moduleManager, developData);
+        this.init();
+    }
+
+    private void init() {
+        Map<EntityID, Set<EntityID>> neighbours = new LazyMap<EntityID, Set<EntityID>>() {
+            @Override
+            public Set<EntityID> createValue() {
+                return new HashSet<>();
+            }
+        };
+        for (Entity next : this.worldInfo) {
+            if (next instanceof Area) {
+                Collection<EntityID> areaNeighbours = ((Area) next).getNeighbours();
+                neighbours.get(next.getID()).addAll(areaNeighbours);
+            }
+        }
+        this.graph = neighbours;
+    }
+
+    @Override
+    public List<EntityID> getResult() {
+        return this.result;
+    }
+
+    @Override
+    public PathPlanning setFrom(EntityID id) {
+        this.from = id;
+        return this;
+    }
+
+  //********Written by Animesh**********//
+	public static int isParseInt(String str){
+	        
+	        int num = -1;
+	        try{
+	             num = Integer.parseInt(str);
+	        } catch (NumberFormatException e) {
+	        }
+	        
+	        return num;
+	    }
+	
+//	public static <T, U> List<U> 
+//    convertStringListToIntList(List<T> listOfString, 
+//                               Function<T, U> function) 
+//    { 
+//        return listOfString.stream() 
+//            .map(function) 
+//            .collect(Collectors.toList()); 
+//    } 
+//	--------------------------------------------------------------------DO NOT EDIT------------------------------------------	
+    @Override
+    public PathPlanning setDestination(Collection<EntityID> targets) {
+//        this.targets = targets;
+//        System.out.println("AgentInfo" + this.agentInfo.getX());
+//    	System.out.println("Building Ids" + this.worldInfo.getAllEntities());
+		 
+		 try {
+//    		 Create a new list that is sent to python
+			 ArrayList<EntityID> newList = new ArrayList<>();
+			 EntityID c1 = new EntityID(959);
+			 newList.add(c1);
+//    			 Open the socket
+			 Socket ss = new Socket("localhost", 2025);
+			 PrintWriter out = new PrintWriter(ss.getOutputStream(), true);
+//    			 Send information to python
+			 out.println(newList);
+//    			 Take the data in from socket
+			 BufferedReader in = new BufferedReader(new InputStreamReader(ss.getInputStream()));
+//    			 Create a string variable that stores the input
+			 String str;
+			 while((str = in.readLine()) != null) 
+			 {
+				 String replace = str.replace("[","");
+				 String replace1 = replace.replace("]","");
+//    				 Split the list
+				 List<String> myList = new ArrayList<String>(Arrays.asList(replace1.split(", ")));
+//    				 Convert string of list to list
+				 List<Integer> listOfInteger = myList.stream().map(s -> Integer.parseInt(s)).collect(Collectors.toList());
+//    				 Print and check if everything is correct
+				 System.out.println("THIS IS THE PARSE INT STRING%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% "+ listOfInteger);
+				 
+//    				 Add the integers to Array - targets1
+				 ArrayList<EntityID> targets1 = new ArrayList<EntityID>();
+				 EntityID a1= new EntityID(listOfInteger.get(0));
+				 targets1.add(a1);
+//				 this.targets = targets1;	 
+//    				 Flushing is important
+				 out.flush();
+			 }
+//    			 Close the connection
+			 out.close();
+			 in.close();
+			 ss.close();
+		 	} catch (FileNotFoundException e) {
+    			// TODO Auto-generated catch block
+    			System.out.println(e);
+    			e.printStackTrace();
+    		} catch (IOException e) {
+    			// TODO Auto-generated catch block
+    			System.out.println(e);
+    			e.printStackTrace();
+    		} 
+		 this.targets = targets;		
+		 return this;
+	 }
+
+//    	--------------------------------------------------------------------DO NOT EDIT------------------------------------------	
+	
+//    		   System.out.println("+++++++++++++++++++++++++++++++++++" + this.worldInfo.getFireBuildingIDs());
+//    		   FireBrigade agent = (FireBrigade) agentInfo.me();
+//    		   EntityID agentID = agentInfo.getID();
+//    		   System.out.println(agentID.getValue() == 1127015061);
+//    		   if (agentID.getValue() == 1127015061) {
+//    			   targets11 = this.worldInfo.getFireBuildingIDs(); 
+//    		   } else {
+//    			   targets22 = this.worldInfo.getFireBuildingIDs();
+//    		   }
+////    		   System.out.println("THIS IS TARGET 11--------------------------------" + targets11);
+////    		   System.out.println("IS TARGET11 null or empty------------------------" + (targets11 == null || targets11.isEmpty()));
+////    		   System.out.println("THIS IS TARGET 22--------------------------------" + targets22);
+////    		   System.out.println("IS TARGET22 null or empty------------------------" + (targets22 == null || targets22.isEmpty()));
+//    		   
+//    		   this.targets = targets;
+//    		   
+//    		   return this;
+//    			--------------------------------------------------------------------DO NOT EDIT------------------------------------------				   
+
+////    		//********Written by Animesh**********//
+//        	ArrayList<EntityID> target_points = new ArrayList<EntityID>();
+//        	EntityID a1 = new EntityID(298);
+//        	target_points.add(a1);
+//            this.targets = target_points;
+//            return this;
+//        	--------------------------------------------------------------------DO NOT EDIT------------------------------------------
+
+    @Override
+    public PathPlanning updateInfo(MessageManager messageManager) {
+        super.updateInfo(messageManager);
+        return this;
+    }
+
+    @Override
+    public PathPlanning precompute(PrecomputeData precomputeData) {
+        super.precompute(precomputeData);
+        return this;
+    }
+
+    @Override
+    public PathPlanning resume(PrecomputeData precomputeData) {
+        super.resume(precomputeData);
+        return this;
+    }
+
+    @Override
+    public PathPlanning preparate() {
+        super.preparate();
+        return this;
+    }
+
+    @Override
+    public PathPlanning calc() {
+        List<EntityID> open = new LinkedList<>();
+        Map<EntityID, EntityID> ancestors = new HashMap<>();
+        open.add(this.from);
+        EntityID next;
+        boolean found = false;
+        ancestors.put(this.from, this.from);
+        do {
+            next = open.remove(0);
+            if (isGoal(next, targets)) {
+                found = true;
+                break;
+            }
+            Collection<EntityID> neighbours = graph.get(next);
+            if (neighbours.isEmpty()) {
+                continue;
+            }
+            for (EntityID neighbour : neighbours) {
+                if (isGoal(neighbour, targets)) {
+                    ancestors.put(neighbour, next);
+                    next = neighbour;
+                    found = true;
+                    break;
+                }
+                else {
+                    if (!ancestors.containsKey(neighbour)) {
+                        open.add(neighbour);
+                        ancestors.put(neighbour, next);
+                    }
+                }
+            }
+        } while (!found && !open.isEmpty());
+        if (!found) {
+            // No path
+            this.result = null;
+        }
+        // Walk back from goal to this.from
+        EntityID current = next;
+        LinkedList<EntityID> path = new LinkedList<>();
+        do {
+            path.add(0, current);
+            current = ancestors.get(current);
+            if (current == null) {
+                throw new RuntimeException("Found a node with no ancestor! Something is broken.");
+            }
+        } while (current != this.from);
+        this.result = path;
+        return this;
+    }
+
+    private boolean isGoal(EntityID e, Collection<EntityID> test) {
+        return test.contains(e);
+    }
+}
---
+++ rcrs-adf-sample-master/src/adf/sample/tactics/SampleTacticsFireBrigade.java
@@ -0,0 +1,296 @@
+package adf.sample.tactics;
+
+import adf.agent.action.Action;
+import adf.agent.action.common.ActionMove;
+import adf.agent.action.common.ActionRest;
+import adf.agent.communication.MessageManager;
+import adf.agent.communication.standard.bundle.centralized.CommandScout;
+import adf.agent.develop.DevelopData;
+import adf.agent.info.AgentInfo;
+import adf.agent.info.ScenarioInfo;
+import adf.agent.info.WorldInfo;
+import adf.agent.module.ModuleManager;
+import adf.agent.precompute.PrecomputeData;
+import adf.debug.WorldViewLauncher;
+import adf.component.centralized.CommandExecutor;
+import adf.component.communication.CommunicationMessage;
+import adf.component.extaction.ExtAction;
+import adf.component.module.complex.Search;
+import adf.sample.tactics.utils.MessageTool;
+import rescuecore2.standard.entities.*;
+import rescuecore2.worldmodel.EntityID;
+import rescuecore2.score.*;
+
+import java.util.List;
+import java.util.Objects;
+
+import adf.agent.action.fire.ActionExtinguish;
+import adf.agent.action.fire.ActionRefill;
+import adf.agent.communication.standard.bundle.centralized.CommandFire;
+import adf.agent.communication.standard.bundle.information.MessageFireBrigade;
+import adf.component.module.complex.BuildingDetector;
+import adf.component.tactics.TacticsFireBrigade;
+
+public class SampleTacticsFireBrigade extends TacticsFireBrigade
+{
+    private BuildingDetector buildingDetector;
+    private Search search;
+
+    private ExtAction actionFireFighting;
+    private ExtAction actionExtMove;
+
+    private CommandExecutor<CommandFire> commandExecutorFire;
+    private CommandExecutor<CommandScout> commandExecutorScout;
+
+    private MessageTool messageTool;
+
+    private CommunicationMessage recentCommand;
+
+	private Boolean isVisualDebug;
+	private ScoreFunction score;
+
+    @Override
+    public void initialize(AgentInfo agentInfo, WorldInfo worldInfo, ScenarioInfo scenarioInfo, ModuleManager moduleManager, MessageManager messageManager, DevelopData developData)
+    {
+        messageManager.setChannelSubscriber(moduleManager.getChannelSubscriber("MessageManager.PlatoonChannelSubscriber", "adf.sample.module.comm.SampleChannelSubscriber"));
+        messageManager.setMessageCoordinator(moduleManager.getMessageCoordinator("MessageManager.PlatoonMessageCoordinator", "adf.sample.module.comm.SampleMessageCoordinator"));
+
+        worldInfo.indexClass(
+                StandardEntityURN.ROAD,
+                StandardEntityURN.HYDRANT,
+                StandardEntityURN.BUILDING,
+                StandardEntityURN.REFUGE,
+                StandardEntityURN.GAS_STATION,
+                StandardEntityURN.AMBULANCE_CENTRE,
+                StandardEntityURN.FIRE_STATION,
+                StandardEntityURN.POLICE_OFFICE
+        );
+
+        this.messageTool = new MessageTool(scenarioInfo, developData);
+
+        this.isVisualDebug = (scenarioInfo.isDebugMode()
+                            && moduleManager.getModuleConfig().getBooleanValue("VisualDebug", false));
+
+        this.recentCommand = null;
+        // init Algorithm Module & ExtAction
+        switch  (scenarioInfo.getMode())
+        {
+            case PRECOMPUTATION_PHASE:
+            case PRECOMPUTED:
+                this.search = moduleManager.getModule("TacticsFireBrigade.Search", "adf.sample.module.complex.SampleSearch");
+                this.buildingDetector = moduleManager.getModule("TacticsFireBrigade.BuildingDetector", "adf.sample.module.complex.SampleBuildingDetector");
+                this.actionFireFighting = moduleManager.getExtAction("TacticsFireBrigade.ActionFireFighting", "adf.sample.extaction.ActionFireFighting");
+                this.actionExtMove = moduleManager.getExtAction("TacticsFireBrigade.ActionExtMove", "adf.sample.extaction.ActionExtMove");
+                this.commandExecutorFire = moduleManager.getCommandExecutor("TacticsFireBrigade.CommandExecutorFire", "adf.sample.centralized.CommandExecutorFire");
+                this.commandExecutorScout = moduleManager.getCommandExecutor("TacticsFireBrigade.CommandExecutorScout", "adf.sample.centralized.CommandExecutorScout");
+                break;
+            case NON_PRECOMPUTE:
+                this.search = moduleManager.getModule("TacticsFireBrigade.Search", "adf.sample.module.complex.SampleSearch");
+                this.buildingDetector = moduleManager.getModule("TacticsFireBrigade.BuildingDetector", "adf.sample.module.complex.SampleBuildingDetector");
+                this.actionFireFighting = moduleManager.getExtAction("TacticsFireBrigade.ActionFireFighting", "adf.sample.extaction.ActionFireFighting");
+                this.actionExtMove = moduleManager.getExtAction("TacticsFireBrigade.ActionExtMove", "adf.sample.extaction.ActionExtMove");
+                this.commandExecutorFire = moduleManager.getCommandExecutor("TacticsFireBrigade.CommandExecutorFire", "adf.sample.centralized.CommandExecutorFire");
+                this.commandExecutorScout = moduleManager.getCommandExecutor("TacticsFireBrigade.CommandExecutorScout", "adf.sample.centralized.CommandExecutorScout");
+                break;
+        }
+
+        registerModule(this.buildingDetector);
+        registerModule(this.search);
+        registerModule(this.actionFireFighting);
+        registerModule(this.actionExtMove);
+        registerModule(this.commandExecutorFire);
+        registerModule(this.commandExecutorScout);
+    }
+
+    @Override
+    public void precompute(AgentInfo agentInfo, WorldInfo worldInfo, ScenarioInfo scenarioInfo, ModuleManager moduleManager, PrecomputeData precomputeData, DevelopData developData)
+    {
+        modulesPrecompute(precomputeData);
+    }
+
+    @Override
+    public void resume(AgentInfo agentInfo, WorldInfo worldInfo, ScenarioInfo scenarioInfo, ModuleManager moduleManager, PrecomputeData precomputeData, DevelopData developData)
+    {
+        modulesResume(precomputeData);
+
+        if (isVisualDebug)
+        {
+            WorldViewLauncher.getInstance().showTimeStep(agentInfo, worldInfo, scenarioInfo);
+        }
+    }
+    
+
+    @Override
+    public void preparate(AgentInfo agentInfo, WorldInfo worldInfo, ScenarioInfo scenarioInfo, ModuleManager moduleManager, DevelopData developData)
+    {
+        modulesPreparate();
+
+        if (isVisualDebug)
+        {
+            WorldViewLauncher.getInstance().showTimeStep(agentInfo, worldInfo, scenarioInfo);
+        }
+    }
+
+    @Override
+    public Action think(AgentInfo agentInfo, WorldInfo worldInfo, ScenarioInfo scenarioInfo, ModuleManager moduleManager, MessageManager messageManager, DevelopData developData)
+    {
+        this.messageTool.reflectMessage(agentInfo, worldInfo, scenarioInfo, messageManager);
+        this.messageTool.sendRequestMessages(agentInfo, worldInfo, scenarioInfo, messageManager);
+        this.messageTool.sendInformationMessages(agentInfo, worldInfo, scenarioInfo, messageManager);
+
+        modulesUpdateInfo(messageManager);
+
+        if (isVisualDebug)
+        {
+            WorldViewLauncher.getInstance().showTimeStep(agentInfo, worldInfo, scenarioInfo);
+        }
+        	
+        FireBrigade agent = (FireBrigade) agentInfo.me();
+        EntityID agentID = agentInfo.getID();
+        // command
+        for (CommunicationMessage message : messageManager.getReceivedMessageList(CommandScout.class))
+        {
+            CommandScout command = (CommandScout) message;
+            if (command.isToIDDefined() && Objects.requireNonNull(command.getToID()).getValue() == agentID.getValue())
+            {
+                this.recentCommand = command;
+                this.commandExecutorScout.setCommand(command);
+            }
+        }
+
+        for (CommunicationMessage message : messageManager.getReceivedMessageList(CommandFire.class))
+        {
+            CommandFire command = (CommandFire) message;
+            if (command.isToIDDefined() && Objects.requireNonNull(command.getToID()).getValue() == agentID.getValue())
+            {
+                this.recentCommand = command;
+                this.commandExecutorFire.setCommand(command);
+            }
+        }
+
+        if (this.recentCommand != null)
+        {
+            Action action = null;
+            if (this.recentCommand.getClass() == CommandFire.class)
+            {
+                action = this.commandExecutorFire.calc().getAction();
+            }
+            else if (this.recentCommand.getClass() == CommandScout.class)
+            {
+                action = this.commandExecutorScout.calc().getAction();
+            }
+
+            if (action != null)
+            {
+            	EntityID a1 = new EntityID(248);
+            	Building building = new Building(a1); 
+				this.sendActionMessage(messageManager, agent, action, agentInfo, worldInfo, score, building);
+                return action;
+            }
+        }
+        // autonomous
+        EntityID target = this.buildingDetector.calc().getTarget();
+        Action action = this.actionFireFighting.setTarget(target).calc().getAction();
+        if (action != null)
+        {
+        	EntityID a1 = new EntityID(248);
+        	Building building = new Building(a1); 
+            this.sendActionMessage(messageManager, agent, action, agentInfo, worldInfo, score, building);
+            return action;
+        }
+        target = this.search.calc().getTarget();
+        action = this.actionExtMove.setTarget(target).calc().getAction();
+        if (action != null)
+        {
+        	EntityID a1 = new EntityID(248);
+        	Building building = new Building(a1); 
+        	this.sendActionMessage(messageManager, agent, action, agentInfo, worldInfo, score, building);
+            return action;
+        }
+
+        messageManager.addMessage(
+                new MessageFireBrigade(true, agent, MessageFireBrigade.ACTION_REST,  agent.getPosition())
+        );
+        return new ActionRest();
+    }
+
+    private void sendActionMessage(MessageManager messageManager, FireBrigade agent, Action action, AgentInfo agentInfo, WorldInfo worldInfo, ScoreFunction score, Building building)
+    {
+        Class<? extends Action> actionClass = action.getClass();
+        int actionIndex = -1;
+        EntityID target = null;
+        if (actionClass == ActionMove.class)
+        {
+            actionIndex = MessageFireBrigade.ACTION_MOVE;
+            List<EntityID> path = ((ActionMove) action).getPath();
+            if (path.size() > 0)
+            {
+            	target = path.get(path.size() - 1);
+                System.out.println("***********Start Moving**************");
+                
+//    			--------------------------------------------------------------------DO NOT EDIT------------------------------------------			
+                
+//                System.out.println("AgentInfo" + agentInfo.getPositionArea());
+//                System.out.println("Building Ids" + worldInfo.getAllEntities());
+//                System.out.println("Building Temp" + building.getFullDescription());
+                FireBrigade agent1 = (FireBrigade) agentInfo.me();
+                EntityID agentID = agentInfo.getID();
+//                System.out.println(agentID.getValue());
+                int buriedness = agent1.getBuriedness();
+                int water = agent1.getWater();
+                int damage = agent1.getDamage();
+                int stamina = agent1.getStamina();
+                int hp1 = agent1.getWater();
+                if (agentID.getValue() == 210552869) {
+                	System.out.println("-----------------------------------------");
+                    System.out.println("THIS THE X: " + agentInfo.getX());
+                    System.out.println("THIS THE Y: " + agentInfo.getY());
+                    System.out.println("THIS THE ID: " + agentInfo.getID());
+                    System.out.println("THIS THE HP: " + hp1);
+                    System.out.println("THIS THE Damage: " + damage);
+                    System.out.println("THIS THE Stamina: " + stamina);
+                    System.out.println("THIS THE Buriedness: " + buriedness);
+                    System.out.println("THIS THE Water level: " + water);
+                    System.out.println("------------------------------------------");
+                } else {
+                	System.out.println("-----------------------------------------");
+                    System.out.println("THIS THE X: " + agentInfo.getX());
+                    System.out.println("THIS THE Y: " + agentInfo.getY());
+                    System.out.println("THIS THE ID: " + agentInfo.getID());
+                    System.out.println("THIS THE HP: " + hp1);
+                    System.out.println("THIS THE Damage: " + damage);
+                    System.out.println("THIS THE Stamina: " + stamina);
+                    System.out.println("THIS THE Buriedness: " + buriedness);
+                    System.out.println("THIS THE Water level: " + water);
+                    System.out.println("------------------------------------------");
+                }
+                
+//    			--------------------------------------------------------------------DO NOT EDIT------------------------------------------                
+                
+            }
+        }
+        else if (actionClass == ActionExtinguish.class)
+        {
+            actionIndex = MessageFireBrigade.ACTION_EXTINGUISH;
+            target = ((ActionExtinguish)action).getTarget();
+            System.out.println("***********Start Extinguishing**************");
+        }
+        else if (actionClass == ActionRefill.class)
+        {
+            actionIndex = MessageFireBrigade.ACTION_REFILL;
+            target = agent.getPosition();
+            System.out.println("***********Start Refilling**************");
+        }
+        else if (actionClass == ActionRest.class)
+        {
+            actionIndex = MessageFireBrigade.ACTION_REST;
+            target = agent.getPosition();
+            System.out.println("***********Start Resting**************");
+        }
+
+        if (actionIndex != -1)
+        {
+            messageManager.addMessage(new MessageFireBrigade(true, agent, actionIndex, target));
+        }
+    }
+}
