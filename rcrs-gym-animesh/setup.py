e73f2de90da1702904b0bd97c3f5a8e5a2717cc0
animeshgoyal9 <43485963+animeshgoyal9@users.noreply.github.com>
2019-10-25 16:47:31 -0500

    Update setup.py

--- rcrs-gym-animesh/setup.py
+++ rcrs-gym-animesh/setup.py
@@ -2,5 +2,5 @@ from setuptools import setup
 
 setup(name='rcrs-gym-animesh',
       version='0.0.1',
-      install_requires=['gym>=0.2.3']
+      install_requires=['gym>=0.2.3', 'pandas', 'cfg_load']
 )
