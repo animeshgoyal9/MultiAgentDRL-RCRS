35e280b6115e8984e37551760b3773bc630ec38b
animeshgoyal9 <43485963+animeshgoyal9@users.noreply.github.com>
2019-10-25 19:04:20 -0500

    Update __init__.py

--- rcrs_gym_animesh/rcrs_gym_animesh/__init__.py
+++ rcrs_gym_animesh/rcrs_gym_animesh/__init__.py
@@ -6,7 +6,4 @@ logger = logging.getLogger(__name__)
 register(
     id='rcrs_gym_animesh-v0',
     entry_point='rcrs_gym_animesh.envs:RCRSEnv',
-    timestep_limit=1000,
-    reward_threshold=1.0,
-    nondeterministic = True,
 )
