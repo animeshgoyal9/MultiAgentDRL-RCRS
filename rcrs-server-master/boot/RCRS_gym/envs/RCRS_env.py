b5a2e84e331fbe73f520ca4c58061d4dbfc12780
animeshgoyal9 <43485963+animeshgoyal9@users.noreply.github.com>
2019-11-27 15:36:57 -0600

    Add files via upload

--- rcrs-server-master/boot/RCRS_gym/envs/RCRS_env.py
+++ rcrs-server-master/boot/RCRS_gym/envs/RCRS_env.py
@@ -30,13 +30,12 @@ MAX_TIMESTEP = 300
 
 action_set_list = np.array([255, 960, 905, 934, 935, 936, 937, 298, 938, 939, 940, 941, 942, 943, 944, 945, 946, 947, 948, 949, 
                     950, 951, 247, 952, 248, 953, 249, 954, 250, 955, 251, 956, 957, 253, 958, 254, 959], dtype = object)
-flag = 0
 
 class RCRSenv(gym.Env):
     metadata = {'render.modes' : None}  
     current_action = 0
     def __init__(self):
-        self.action_space = spaces.Discrete(37)
+        self.action_space = spaces.MultiDiscrete([37, 37])
         
         low = np.array([0]*79)
         high = np.array([inf]*79)
@@ -52,7 +51,7 @@ class RCRSenv(gym.Env):
         logging.basicConfig()
         
         self.curr_episode += 1
-        if (self.curr_episode <= 299):
+        if (self.curr_episode <= (MAX_TIMESTEP - 1)):
             self.reward = 0
         else:
             self.reward = run_reward()
@@ -65,26 +64,29 @@ class RCRSenv(gym.Env):
         
         self.state = flat_list
 
-        self.current_action = action
+        self.current_action_1 = action[0]
+        self.current_action_2 = action[1]
 
-        # print("Current Action: ", self.current_action)
+        # print("Current Action_1: ", self.current_action_1)
+        # print("Current Action_2: ", self.current_action_2)
         # print("Current State: ", self.state)
         print("Current Episode: ", self.curr_episode)
         
         done = bool(self.curr_episode == MAX_TIMESTEP)
         if done == True:
-            os.system("for pid in $(ps -ef | grep 'start-comprun'); do kill -2 $pid; done")
-
-        # time.sleep(0.137)
-        time.sleep(1)
+            subprocess.Popen("/u/animesh9/Documents/RoboCup-gRPC/rcrs-server-master/boot/kill.sh", shell=True)
+        print("******************************")
+        time.sleep(0.119)
+        # time.sleep(1.1)
+        # int(input("pause.."))
         return np.array(self.state), self.reward, done , {}
 
     def reset(self):
-        # time.sleep(0.5)
-        subprocess.call(['gnome-terminal', '-e', "python3 /u/animesh9/Documents/RoboCup-gRPC/rcrs-server-master/boot/delete_master.py"])
-        time.sleep(12)
+        # time.sleep(2)
+        subprocess.call(['gnome-terminal', '-e', "python3 /u/animesh9/Documents/RoboCup-gRPC/rcrs-server-master/boot/launch_file.py"])
+        time.sleep(11.5)
         self.curr_episode = 0
-        reset_action = 0
+        reset_action = [0, 0]
         reset = []
 
         reset.append(run_server())
@@ -104,20 +106,13 @@ def run_adf(bid):
     # NOTE(gRPC Python Team): .close() is possible on a channel and should be
     # used in circumstances in which the with statement does not fit the needs
     # of the code.
-    with grpc.insecure_channel('localhost:9090') as channel:
+    
+    with grpc.insecure_channel('localhost:3400') as channel:
         stub = AgentInfo_pb2_grpc.AnimFireChalAgentStub(channel)
-        print("Current action: ", action_set_list[bid])
-
-        flag = 1-flag
-        
-        if (flag==1):
-            aid = 210552869
-        else:
-            aid = 1962675462
-
-        print("Current Agent id: ", aid)
+        print("Current action_1 for 210552869: ", action_set_list[bid[0]])
+        print("Current action_2 for 1962675462: ", action_set_list[bid[1]])
         response = stub.getAgentInfo(AgentInfo_pb2.ActionInfo(actions = [
-            AgentInfo_pb2.Action(agent_id = 210552869, building_id=action_set_list[bid]), AgentInfo_pb2.Action(agent_id = 1962675462, building_id=action_set_list[len(action_set_list) - bid -1])]))
+            AgentInfo_pb2.Action(agent_id = 210552869, building_id=action_set_list[bid[0]]), AgentInfo_pb2.Action(agent_id = 1962675462, building_id=action_set_list[bid[1]])]))
             # AgentInfo_pb2.Action(agent_id = aid, building_id=action_set_list[bid])]))
     # print(response.agents)
     agent_state_info = []
