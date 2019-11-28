55c9c1fe7e88869c2985e28abc068c8bca1f0eaf
aasthagoyal46@gmail.com <animesh9@cs.utexas.edu>
2019-11-27 20:42:22 -0600

    delete extra

--- rcrs-server-master/boot/delete1.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-import time, os 
-import signal, sys
-
- 
-# subprocess.call(["sh", "kill.sh"])
-
-subprocess.Popen("/u/animesh9/Documents/RoboCup-gRPC/rcrs-server-master/boot/start-comprun.sh", shell=True)
-time.sleep(30)
