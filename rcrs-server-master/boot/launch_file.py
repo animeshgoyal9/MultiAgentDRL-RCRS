31323e0d18dac5855cba0af0ea9c966f79b788d5
animeshgoyal9 <43485963+animeshgoyal9@users.noreply.github.com>
2020-01-23 20:28:48 -0600

    Update launch_file.py

--- rcrs-server-master/boot/launch_file.py
+++ rcrs-server-master/boot/launch_file.py
@@ -18,7 +18,7 @@ import signal, sys
 subprocess.Popen("bash 'start-comprun.sh'", shell=True)
 time.sleep(10)
 subprocess.Popen("/u/animesh9/Documents/RoboCup-gRPC/rcrs-adf-sample/launch.sh '-all'", shell=True)
-time.sleep(500)	
+time.sleep(500000)	
 
 # subprocess.call(['python3', '/u/animesh9/Documents/RoboCup-gRPC/rcrs-server-master/boot/delete.py'])
 
