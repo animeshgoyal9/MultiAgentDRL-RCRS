2d9f030f1f540793cc2dc4b868c2d73410ae9d21
animeshgoyal9 <43485963+animeshgoyal9@users.noreply.github.com>
2020-02-14 17:04:43 -0600

    Add files via upload

--- rcrs-server-master/boot/testing.py
+++ rcrs-server-master/boot/testing.py
@@ -1,6 +1,14 @@
 import gym
 import RCRS_gym
 
+
+import sys
+sys.path.append("/u/animesh9/Documents/MultiAgentDRL-RCRS/PyRCRSClient/RCRS_gRPC_Client")
+import AgentInfo_pb2
+import AgentInfo_pb2_grpc
+import BuildingInfo_pb2
+import BuildingInfo_pb2_grpc
+
 import os
 import numpy as np
 import shutil
@@ -13,8 +21,8 @@ from datetime import date, datetime
 import subprocess
 from subprocess import *
 
-from stable_baselines.common.policies import MlpPolicy, MlpLstmPolicy, FeedForwardPolicy
-# from stable_baselines.deepq.policies import MlpPolicy
+# from stable_baselines.common.policies import MlpPolicy, MlpLstmPolicy, FeedForwardPolicy
+from stable_baselines.deepq.policies import MlpPolicy
 from stable_baselines.common.vec_env import DummyVecEnv, VecNormalize, VecEnv
 from stable_baselines import PPO2, DQN, A2C
 # from stable_baselines.common.evaluation import evaluate_policy
@@ -29,32 +37,21 @@ import warnings
 warnings.filterwarnings("ignore", category=DeprecationWarning) 
 
 
-# Create log dir
-# log_dir = "/u/animesh9/Documents/MultiAgentDRL-RCRS//plots/"
-# os.makedirs(log_dir, exist_ok=True)
-# Create and wrap the environment
 env = gym.make('RCRS-v2')
-# env = Monitor(env, allow_early_resets=True)
 # The algorithms require a vectorized environment to run
 env = DummyVecEnv([lambda: env]) 
 # Automatically normalize the input features
 env = VecNormalize(env, norm_obs=True, norm_reward=False, clip_obs=10.)
 
-# Add some param noise for exploration
-# param_noise = AdaptiveParamNoiseSpec(initial_stddev=0.1, desired_action_stddev=0.1)
 # Because we use parameter noise, we should use a MlpPolicy with layer normalization
 
-now = datetime.now()
-dt_string = now.strftime("%d/%m/%Y")
 columns = ['Mean Rewards', 'Standard deviation']
 df = pd.DataFrame(columns=columns)
 
 total_timesteps_to_learn =      2500 # 50 episodes
 total_timesteps_to_predict =    2500 # 50 episodes
-algo_used =                     "A2C"
+algo_used =                     "DQN"
 
-
-# Custom MLP policy of three layers of size 128 each
 class CustomPolicy(FeedForwardPolicy):
     def __init__(self, *args, **kwargs):
         super(CustomPolicy, self).__init__(*args, **kwargs,
@@ -63,11 +60,170 @@ class CustomPolicy(FeedForwardPolicy):
                                            feature_extraction="mlp")
 
 
+    def learn(self, total_timesteps, callback=None, log_interval=100, tb_log_name="DQN",
+              reset_num_timesteps=True, replay_wrapper=None):
+
+        new_tb_log = self._init_num_timesteps(reset_num_timesteps)
+
+        with SetVerbosity(self.verbose), TensorboardWriter(self.graph, self.tensorboard_log, tb_log_name, new_tb_log) \
+                as writer:
+            self._setup_learn()
+
+            # Create the replay buffer
+            if self.prioritized_replay:
+                self.replay_buffer = PrioritizedReplayBuffer(self.buffer_size, alpha=self.prioritized_replay_alpha)
+                if self.prioritized_replay_beta_iters is None:
+                    prioritized_replay_beta_iters = total_timesteps
+                else:
+                    prioritized_replay_beta_iters = self.prioritized_replay_beta_iters
+                self.beta_schedule = LinearSchedule(prioritized_replay_beta_iters,
+                                                    initial_p=self.prioritized_replay_beta0,
+                                                    final_p=1.0)
+            else:
+                self.replay_buffer = ReplayBuffer(self.buffer_size)
+                self.beta_schedule = None
+
+            if replay_wrapper is not None:
+                assert not self.prioritized_replay, "Prioritized replay buffer is not supported by HER"
+                self.replay_buffer = replay_wrapper(self.replay_buffer)
+
+            # Create the schedule for exploration starting from 1.
+            self.exploration = LinearSchedule(schedule_timesteps=int(self.exploration_fraction * total_timesteps),
+                                              initial_p=self.exploration_initial_eps,
+                                              final_p=self.exploration_final_eps)
+
+            episode_rewards = [0.0]
+            episode_successes = []
+            obs = self.env.reset()
+            reset = True
+            F = 0
+
+
+            for _ in range(total_timesteps):
+                if callback is not None:
+                    # Only stop training if return value is False, not when it is None. This is for backwards
+                    # compatibility with callbacks that have no return statement.
+                    if callback(locals(), globals()) is False:
+                        break
+                # Take action and update exploration to the newest value
+                kwargs = {}
+                if not self.param_noise:
+                    update_eps = self.exploration.value(self.num_timesteps)
+                    update_param_noise_threshold = 0.
+                else:
+                    update_eps = 0.
+                    # Compute the threshold such that the KL divergence between perturbed and non-perturbed
+                    # policy is comparable to eps-greedy exploration with eps = exploration.value(t).
+                
+                    update_param_noise_threshold = \
+                        -np.log(1. - self.exploration.value(self.num_timesteps) +
+                                self.exploration.value(self.num_timesteps) / float(self.env.action_space.n))
+                    kwargs['reset'] = reset
+                    kwargs['update_param_noise_threshold'] = update_param_noise_threshold
+                    kwargs['update_param_noise_scale'] = True
+
+                with self.sess.as_default():
+                    action = self.act(np.array(obs)[None], update_eps=update_eps, **kwargs)[0]
+
+                # Check if agent is busy or idle
+                while (run_adf(action) == 0):
+                    env_action = action
+                    reset = False
+                    new_obs, rew, done, info = self.env.step(env_action)
+                    F = F + rew         
+
+                # Store transition in the replay buffer.
+                self.replay_buffer.add(obs, action, F, new_obs, float(done))
+                obs = new_obs
+
+                if writer is not None:
+                    ep_rew = np.array([F]).reshape((1, -1))
+                    ep_done = np.array([done]).reshape((1, -1))
+                    total_episode_reward_logger(self.episode_reward, ep_rew, ep_done, writer,
+                                                self.num_timesteps)
+
+                episode_rewards[-1] += F
+
+                if done:
+                    maybe_is_success = info.get('is_success')
+                    if maybe_is_success is not None:
+                        episode_successes.append(float(maybe_is_success))
+                    if not isinstance(self.env, VecEnv):
+                        obs = self.env.reset()
+                    episode_rewards.append(0.0)
+                    reset = True
+
+                # Do not train if the warmup phase is not over
+                # or if there are not enough samples in the replay buffer
+                can_sample = self.replay_buffer.can_sample(self.batch_size)
+                if can_sample and self.num_timesteps > self.learning_starts \
+                        and self.num_timesteps % self.train_freq == 0:
+                    # Minimize the error in Bellman's equation on a batch sampled from replay buffer.
+                    # pytype:disable=bad-unpacking
+                    if self.prioritized_replay:
+                        assert self.beta_schedule is not None, \
+                               "BUG: should be LinearSchedule when self.prioritized_replay True"
+                        experience = self.replay_buffer.sample(self.batch_size,
+                                                               beta=self.beta_schedule.value(self.num_timesteps))
+                        (obses_t, actions, rewards, obses_tp1, dones, weights, batch_idxes) = experience
+                    else:
+                        obses_t, actions, rewards, obses_tp1, dones = self.replay_buffer.sample(self.batch_size)
+                        weights, batch_idxes = np.ones_like(rewards), None
+                    # pytype:enable=bad-unpacking
+
+                    if writer is not None:
+                        # run loss backprop with summary, but once every 100 steps save the metadata
+                        # (memory, compute time, ...)
+                        if (1 + self.num_timesteps) % 100 == 0:
+                            run_options = tf.RunOptions(trace_level=tf.RunOptions.FULL_TRACE)
+                            run_metadata = tf.RunMetadata()
+                            summary, td_errors = self._train_step(obses_t, actions, rewards, obses_tp1, obses_tp1,
+                                                                  dones, weights, sess=self.sess, options=run_options,
+                                                                  run_metadata=run_metadata)
+                            writer.add_run_metadata(run_metadata, 'step%d' % self.num_timesteps)
+                        else:
+                            summary, td_errors = self._train_step(obses_t, actions, rewards, obses_tp1, obses_tp1,
+                                                                  dones, weights, sess=self.sess)
+                        writer.add_summary(summary, self.num_timesteps)
+                    else:
+                        _, td_errors = self._train_step(obses_t, actions, rewards, obses_tp1, obses_tp1, dones, weights,
+                                                        sess=self.sess)
+
+                    if self.prioritized_replay:
+                        new_priorities = np.abs(td_errors) + self.prioritized_replay_eps
+                        assert isinstance(self.replay_buffer, PrioritizedReplayBuffer)
+                        self.replay_buffer.update_priorities(batch_idxes, new_priorities)
+
+                if can_sample and self.num_timesteps > self.learning_starts and \
+                        self.num_timesteps % self.target_network_update_freq == 0:
+                    # Update target network periodically.
+                    self.update_target(sess=self.sess)
+
+                if len(episode_rewards[-101:-1]) == 0:
+                    mean_100ep_reward = -np.inf
+                else:
+                    mean_100ep_reward = round(float(np.mean(episode_rewards[-101:-1])), 1)
+
+                num_episodes = len(episode_rewards)
+                if self.verbose >= 1 and done and log_interval is not None and len(episode_rewards) % log_interval == 0:
+                    logger.record_tabular("steps", self.num_timesteps)
+                    logger.record_tabular("episodes", num_episodes)
+                    if len(episode_successes) > 0:
+                        logger.logkv("success rate", np.mean(episode_successes[-100:]))
+                    logger.record_tabular("mean 100 episode reward", mean_100ep_reward)
+                    logger.record_tabular("% time spent exploring",
+                                          int(100 * self.exploration.value(self.num_timesteps)))
+                    logger.dump_tabular()
+
+                self.num_timesteps += 1
+
+        return self
+
 # for i in range(2):
 # model = DQN(MlpPolicy, env, verbose=1, learning_rate=0.0025, tensorboard_log = "./ppo2_rcrs_tensorboard/", batch_size = 64)
-model = A2C(CustomPolicy, env, verbose=1, learning_rate=0.0025, tensorboard_log = "./ppo2_rcrs_tensorboard/", n_steps = 256)
-# model = PPO2.load("rcrs_wgts_18_PPO2.pkl")
-# obs = env.reset()
+model = DQN(CustomPolicy, env, verbose=1, learning_rate=0.0025, tensorboard_log = "./ppo2_rcrs_tensorboard/", n_steps = 256)
+
+
 
 for k in range(25):
     # Train the agent
@@ -81,7 +237,7 @@ for k in range(25):
 
 for j in range(25):
     # Load the trained agent
-    model = A2C.load("{}_{}_{}".format("rcrs_wgts", j, algo_used))
+    model = DQN.load("{}_{}_{}".format("rcrs_wgts", j, algo_used))
     # Reset the environment
     obs = env.reset()
     # Create an empty list to store reward values 
@@ -98,14 +254,28 @@ for j in range(25):
     print(np.std(final_rewards))
     # Create a DataFrame to save the mean and standard deviation
     df = df.append({'Mean Rewards': np.mean(final_rewards), 'Standard deviation': np.std(final_rewards)}, ignore_index=True)
-    # # Create a dataframe to save the mean and standard deviation
-    # df2 = pd.DataFrame([np.mean(final_rewards), stats.sem(final_rewards)], index = ['Rewards', 'Standard Error'])
-    # Convert to csv
+    
     df.to_csv("{}_{}_{}".format(1, algo_used, "MeanAndStdReward.csv", sep=',',index=True))
-    # # Convert to excel
-    # df2.to_excel("{}_{}_{}".format(j+1, algo_used, "MeanAndStdReward.xlsx" ))
-
+    
     subprocess.Popen("/u/animesh9/Documents/MultiAgentDRL-RCRS/rcrs-server-master/boot/kill.sh", shell=True)
 
     # Kill the process once training and testing is done
 subprocess.Popen("/u/animesh9/Documents/MultiAgentDRL-RCRS/rcrs-server-master/boot/kill.sh", shell=True)
+
+
+
+# Run gRPC server
+def run_adf(bid):
+    with grpc.insecure_channel('localhost:3902') as channel:
+        stub = AgentInfo_pb2_grpc.AnimFireChalAgentStub(channel)
+        response = stub.getAgentInfo(AgentInfo_pb2.ActionInfo(actions = [
+            AgentInfo_pb2.Action(agent_id = 2090075220, building_id=action_set_list[bid[0]]), 
+            AgentInfo_pb2.Action(agent_id = 1618773504, building_id=action_set_list[bid[1]]),
+            AgentInfo_pb2.Action(agent_id = 1127234487, building_id=action_set_list[bid[2]]), 
+            AgentInfo_pb2.Action(agent_id = 1535509101, building_id=action_set_list[bid[3]])]))
+            
+    agent_state_info = []
+
+    for i in response.agents:
+        agent_state_info.append(i.idle)
+    return agent_state_info
